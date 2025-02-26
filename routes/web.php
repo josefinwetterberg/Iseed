6b44d4a3b2d6237bf90075064a25231d07710f9f
PU-MEriksson <malin.eriksson83@icloud.com>
2025-02-26 15:46:14 +0100

    Added dashboard and login-page

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+//})->middleware(['auth'])->name('dashboard');
