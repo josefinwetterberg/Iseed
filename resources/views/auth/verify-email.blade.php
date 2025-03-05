7cb599cd496240594b12dd849488fb43c8735ff7
PU-MEriksson <malin.eriksson83@icloud.com>
2025-03-05 10:01:22 +0100

    Continued removing tailwind from blade files

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,20 +1,20 @@
 <x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
+    <div class="auth-message">
         {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
     </div>
 
     @if (session('status') == 'verification-link-sent')
-        <div class="mb-4 font-medium text-sm text-green-600">
+        <div class="status-message status-success">
             {{ __('A new verification link has been sent to the email address you provided during registration.') }}
         </div>
     @endif
 
-    <div class="mt-4 flex items-center justify-between">
+    <div class="form-actions-between">
         <form method="POST" action="{{ route('verification.send') }}">
             @csrf
 
             <div>
-                <x-primary-button>
+                <x-primary-button class="button-primary">
                     {{ __('Resend Verification Email') }}
                 </x-primary-button>
             </div>
@@ -23,7 +23,7 @@
         <form method="POST" action="{{ route('logout') }}">
             @csrf
 
-            <button type="submit" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            <button type="submit" class="button-link">
                 {{ __('Log Out') }}
             </button>
         </form>
