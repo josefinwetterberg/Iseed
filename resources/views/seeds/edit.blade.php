0a22373d44217e7c670dce590e0b04bc7c2419dc
Josefinx <josefin.wetterberg@gmail.com>
2025-03-23 13:41:53 +0100

    added some a11y changes

--- resources/views/seeds/edit.blade.php
+++ resources/views/seeds/edit.blade.php
@@ -1,6 +1,8 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2>Edit Seed</h2>
+        <h1>Edit Seed</h1>
+        {{-- Add link to go back to dashboard --}}
+        <a href="{{ route('dashboard') }}" class="underline">Back to dashboard</a>
     </x-slot>
 
     <div class="edit-container">
